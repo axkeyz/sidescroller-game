b1e040e4b8ea2a4934912e8f50a143fc2c132472
axkeyz <xahkun@gmail.com>
2022-08-04 13:23:34 +1200

    Initial commit

---
+++ Scenes/Hero/HeroMovement.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+func _physics_process(delta):
+	move_x()
+
+func move_x():
+#	var path_position = get_global_mouse_position()
+#	get_node("CombatSprite").position.x = path_position.x
+	pass
---
+++ Scenes/Map/ParallaxBackground.gd
@@ -0,0 +1,5 @@
+extends ParallaxBackground
+
+
+func _physics_process(delta):
+	scroll_base_offset -= Vector2(100,0) * delta
