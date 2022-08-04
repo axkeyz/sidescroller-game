087ff549c86ef7e6bd8468c39bc3065b499bc652
axkeyz <xahkun@gmail.com>
2022-08-04 18:00:44 +1200

    Update Map-Hero interactions

--- Scenes/Hero/HeroMovement.gd
+++ Scenes/Hero/HeroMovement.gd
@@ -1,9 +1,17 @@
 extends KinematicBody2D
 
-func _physics_process(delta):
-	move_x()
+var POS_Y = 950
 
-func move_x():
-#	var path_position = get_global_mouse_position()
-#	get_node("CombatSprite").position.x = path_position.x
+func _ready():
+	get_node("CombatSprite").position.y = POS_Y
+
+func _physics_process(delta):
+	move_x(delta)
+
+func move_x(delta):
+	var path_position = get_global_mouse_position()
+	var path_position_x = Vector2(clamp(path_position.x, 120, 1800), POS_Y)
+	# TODO: Clamp 1800 -> "safe" distance from enemy
+	get_node("CombatSprite").position = get_node("CombatSprite").position.linear_interpolate(path_position_x, delta * 0.5)
+#	get_node("CombatSprite").position.x = clamp(path_position.x, 120, 1800)
 	pass
--- Scenes/Map/Test/TestMapParallaxScroller.gd
+++ Scenes/Map/Test/TestMapParallaxScroller.gd
@@ -1,6 +1,7 @@
 extends ParallaxBackground
 
-export (float) var scrolling_speed = 120.0
+#export (float) var scrolling_speed = 120.0
 
 func _physics_process(delta):
+	var scrolling_speed = get_node("Baxton").get("player_speed")
 	scroll_base_offset.x -= scrolling_speed * delta
