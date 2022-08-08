28f1e09159512be944d96d9bc4cc8e7d7e5112e4
axkeyz <xahkun@gmail.com>
2022-08-08 23:21:03 +1200

    Update project settings

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 func _ready():
 	get_node("StartMenu/Background").connect("pressed", self, "on_startgame_pressed")
+	get_node("StartMenu/Background/M/VB/HB/VBRight/LogoutButton").connect("pressed", self, "on_end_game_pressed")
 
 func on_startgame_pressed():
 	# Remove StartMenu
@@ -10,3 +11,7 @@ func on_startgame_pressed():
 	# Add Lobby Scene
 	var lobby_scene = load("res://Scenes/Main/LobbyScene.tscn").instance()
 	add_child(lobby_scene)
+
+func on_end_game_pressed():
+	# Remove all nodes and quit
+	get_tree().quit()
