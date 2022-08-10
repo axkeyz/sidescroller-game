dd1c1a14e0ed0a674d749750579a5cd4ca0c2d0a
axkeyz <xahkun@gmail.com>
2022-08-11 01:18:49 +1200

    Add utility singleton

---
+++ Singletons/Utils.gd
@@ -0,0 +1,10 @@
+extends Node
+
+class_name Utils
+
+#static func get_all_children_names(node):
+#	var names = []
+#	for child in node.get_children():
+#		names.append(child.name)
+#
+#	return names
