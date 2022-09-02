4707a29f059539c790a2e597830a4ba1137c9f7a
axkeyz <xahkun@gmail.com>
2022-09-02 14:47:00 +1200

    Add user settings resources

---
+++ Singletons/Resources/UnitsInfo.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+export var units = {
+	"Baxton": {
+		"folder": "res://Assets/Heroes/Baxton/",
+		"voicelines": {
+			"special": "Got stuck? I can carry you", 
+			"custom": "This game is kinda fun", 
+		}
+	}
+}
---
+++ Singletons/Resources/UserSettings.gd
@@ -0,0 +1,50 @@
+extends Resource
+
+export var lobby_characters = [
+	{
+		"character": "Baxton",
+		"expression": "default",
+		"position": [0.0, 0.0],
+		"scale": 1.0
+	}
+]
+
+export var game_quality = {
+	"fps": 60,
+	"quality": "medium",
+	"gpu": "enable"
+}
+
+export var sound = {
+	"music": 50,
+	"sfx": 50,
+	"ui": 50,
+	"voices": 0,
+	"enable_all": true,
+	"notifications": {
+		"enable_all": true,
+		"craft": true,
+		"dispatch": true,
+		"chat": true,
+	}
+}
+
+export var language = {
+	"voice": "none",
+	"text": "english",
+}
+
+export var battle = {
+	"auto": false,
+	"auto_special": false,
+	"show_damage": true,
+	"show_buffs": true,
+	"show_special": true,
+	"tracking": true,
+	"show_advantages": true,
+}
+
+export var user = {
+	"streaming settings": false,
+	"gacha_warning": true,
+}
