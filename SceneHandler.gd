65e389b2406ae0307e4e70adde8ccbee514e1996
axkeyz <xahkun@gmail.com>
2022-09-03 15:13:54 +1200

    Initialise Nakama auth

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
-func _ready():
+onready var server_connection := $ServerConnection
+onready var debug_panel := $StartMenu/Background/M/VB/HB/VBLeft/ServerLabel
+
+func _ready() -> void:
 # warning-ignore:return_value_discarded
 	get_node("StartMenu/Background").connect("pressed", self, "on_startgame_pressed")
 # warning-ignore:return_value_discarded
 	get_node("StartMenu/Background/M/VB/HB/VBRight/LogoutButton").connect("pressed", self, "on_end_game_pressed")
+	
+	# Connect nakama server
+	var email := "test@test.com"
+	var password := "password"
+	
+	debug_panel.text  = "Authenticating %s" % email
+	var result : int = yield(server_connection.authenticate_async(email, password), "completed")
+	
+	if result == OK:
+		debug_panel.text = "Authenticated user successfully"
+	else:
+		debug_panel.text = "Failed to authenticate user"
+
 
 func on_startgame_pressed():
 	# Remove StartMenu
---
+++ ServerConnection.gd
@@ -0,0 +1,20 @@
+extends Node
+
+const KEY := "axkeyz_sidescroller"
+
+var _session : NakamaSession
+
+var _client := Nakama.create_client(KEY, "127.0.0.1", 7350, "http")
+
+func authenticate_async(email: String, password: String) -> int:
+	var result := OK
+	
+	var new_session : NakamaSession = yield(
+		_client.authenticate_email_async(email, password, null, false), "completed")
+	
+	if not new_session.is_exception():
+		_session = new_session
+	else:
+		result = new_session.get_exception().status_code
+	
+	return result
