adffc16ec93c8f5dd6f89cbec9993fbac003b4c6
axkeyz <xahkun@gmail.com>
2022-09-16 22:04:37 +1200

    Add condition to movement

--- Scenes/Hero/HeroMovement.gd
+++ Scenes/Hero/HeroMovement.gd
@@ -1,16 +1,21 @@
 extends KinematicBody2D
 
-var POS_Y = 950
+export (int) var pos_y = 950
+export (int) var safe_x = 80
+
+var is_deployed = false
 
 func _ready():
-	get_node("CombatSprite").position.y = POS_Y
+	if is_deployed:
+		get_node("CombatSprite").position.y = pos_y
 
 func _physics_process(delta):
-	move_x(delta)
+	if is_deployed:
+		run(delta)
 
-func move_x(delta):
+func run(delta):
 	var path_position = get_global_mouse_position()
-	var path_position_x = Vector2(clamp(path_position.x, 120, 1800), POS_Y)
+	var path_position_x = Vector2(clamp(path_position.x, 120, 1800), pos_y)
 	# TODO: Clamp 1800 -> "safe" distance from enemy
 	get_node("CombatSprite").position = get_node("CombatSprite").position.linear_interpolate(path_position_x, delta * 0.5)
 #	get_node("CombatSprite").position.x = clamp(path_position.x, 120, 1800)
