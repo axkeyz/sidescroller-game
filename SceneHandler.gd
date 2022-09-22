d35fea1f7b0da8ca227a5290c1bbd12d6bf68c4a
axkeyz <xahkun@gmail.com>
2022-09-22 23:24:30 +1200

    Create auth for guest accounts

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var user := load("res://Resources/User/UserDetails.tres")
+
 onready var server_connection := $ServerConnection
 onready var debug_panel := $StartMenu/Background/M/VB/HB/VBLeft/ServerLabel
 
@@ -10,8 +12,20 @@ func _ready() -> void:
 	get_node("StartMenu/Background/M/VB/HB/VBRight/LogoutButton").connect("pressed", self, "on_end_game_pressed")
 	
 	# Connect nakama server
-	var email := "test@test.com"
-	var password := "password"
+	authenticate_user()
+
+func authenticate_user():
+	# Connect nakama server
+	var email : String = user.identity["email"]
+	
+	if email != "":
+		authenticate_existing_nakama_user(email)
+	else:
+		reauthenticate_or_register_user()
+	
+
+func authenticate_existing_nakama_user(email: String) -> void:
+	var password : String = user.identity["password"]
 	
 	debug_panel.text  = "Authenticating %s" % email
 	var result : int = yield(server_connection.authenticate_async(email, password), "completed")
@@ -20,7 +34,30 @@ func _ready() -> void:
 		debug_panel.text = "Authenticated user successfully"
 	else:
 		debug_panel.text = "Failed to authenticate user"
+	
 
+func reauthenticate_or_register_user() -> void:
+	register_guest_user()
+	## TODO: Add login
+	pass
+
+func register_guest_user() -> void:
+	debug_panel.text = "Creating guest user..."
+	
+	save_guest_user_details_to_local()
+	
+	var result : int = yield(server_connection.guest_login_async(user.identity["id"]), "completed")
+	
+	if result == OK:
+		debug_panel.text = "Created guest user successfully"
+	else:
+		debug_panel.text = "Failed to authenticate user"
+	pass
+
+func save_guest_user_details_to_local() -> void:
+	user.identity["id"] = "user#%s" % Utils.generate_unique_string(8)
+	user.identity["device"].append(OS.get_unique_id())
+	ResourceSaver.save("res://Resources/User/UserDetails.tres", user)
 
 func on_startgame_pressed():
 	# Remove StartMenu
--- ServerConnection.gd
+++ ServerConnection.gd
@@ -6,11 +6,26 @@ var _session : NakamaSession
 
 var _client := Nakama.create_client(KEY, "127.0.0.1", 7350, "http")
 
+func guest_login_async(username : String) -> int:
+	var result := OK
+	
+	var new_session : NakamaSession = yield(
+		_client.authenticate_device_async(OS.get_unique_id(), username), "completed"
+	)
+	
+	if not new_session.is_exception():
+		_session = new_session
+	else:
+		result = new_session.get_exception().status_code
+
+	return result
+
 func authenticate_async(email: String, password: String) -> int:
 	var result := OK
 	
 	var new_session : NakamaSession = yield(
-		_client.authenticate_email_async(email, password, null, false), "completed")
+		_client.authenticate_email_async(email, password, null, false), "completed"
+	)
 	
 	if not new_session.is_exception():
 		_session = new_session
