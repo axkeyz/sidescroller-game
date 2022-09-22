2019344764dd0fcbdb3a96e03cadc0cab392cb06
axkeyz <xahkun@gmail.com>
2022-09-22 21:23:02 +1200

    Fix deploy limit
    - Fixed an issue where units could be redeployed regardless of
    deployment resource

--- Scenes/Main/BattleScene.gd
+++ Scenes/Main/BattleScene.gd
@@ -5,16 +5,16 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	$Baxton.connect("deploy_success", self, "change_DeployResources")
 	pass # Replace with function body.
 
 func _on_DeployButton_gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch:
 		if not event.pressed:
-			$Baxton.deploy($Baxton.deploy_cost, event.position.x)
+			$Baxton.deploy($BattleUI.deploy_resources, event.position.x)
 	pass # Replace with function body.
 
-func change_DeployResources():
+func change_DeployResources() -> void:
 	$Baxton.show()
 	$BattleUI.start_DeployResources($Baxton.deploy_cost)
