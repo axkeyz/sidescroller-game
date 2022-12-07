cf2d07b0fea194c8934b61ed2a109a00c723c08b
axkeyz <xahkun@gmail.com>
2022-12-07 19:20:34 +1300

    Add static typing

--- Scenes/UI/PCStatsContainer.gd
+++ Scenes/UI/PCStatsContainer.gd
@@ -4,22 +4,22 @@ var datetime
 var date
 var time
 
-func _ready():
+func _ready() -> void:
 	update_date()
 	update_datetime_label()
 
-func _physics_process(_delta):
+func _physics_process(_delta) -> void:
 	update_datetime_label()
 	
 	$BatteryLabel.value = OS.get_power_percent_left()
 
-func update_date():
+func update_date() -> void:
 	datetime = OS.get_datetime()
 	
 	date = "%02d/%02d/%04d" % [datetime["day"], datetime["month"],
 	datetime["year"]]
 
-func update_datetime_label():
+func update_datetime_label() -> void:
 	datetime = OS.get_time()
 	
 	time = "%02d:%02d" % [datetime["hour"], datetime["minute"]]
